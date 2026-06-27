43efba3f1e3badda9c85c2cc05a664e2dd4378f3
Abneesh Patel <abneeshpatel9@gmail.com>
2026-06-28 05:26:50 +0530

    updated cors

--- server/src/server.js
+++ server/src/server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: (origin, callback) => callback(null, true),
+    origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
